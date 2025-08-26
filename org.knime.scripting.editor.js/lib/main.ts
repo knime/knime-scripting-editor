9f7ed338c7815bec2df155a668917e5f1218482a
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-08-26 14:42:12 +0200

    AP-24709: Move getSettingsService to `init.ts`
    AP-24709 (Do not initialize on import in @knime/scripting-editor but export async initialize function)

--- org.knime.scripting.editor.js/lib/main.ts
+++ org.knime.scripting.editor.js/lib/main.ts
@@ -34,6 +34,7 @@ import editor from "@/editor";
 import {
   type InitMockData,
   getScriptingService,
+  getSettingsService,
   init,
   initMocked,
 } from "@/init";
@@ -49,10 +50,7 @@ import {
   type ScriptingServiceType,
   initConsoleEventHandler,
 } from "@/scripting-service";
-import {
-  type GenericNodeSettings,
-  getSettingsService,
-} from "@/settings-service";
+import { type GenericNodeSettings } from "@/settings-service";
 import { setActiveEditorStoreForAi } from "@/store/ai-bar";
 import { useReadonlyStore } from "@/store/readOnly";
 
